7e594090f5eade167cf6db8b737401466efe4798
LiZifan <zifanli@umich.edu>
2015-08-26 23:16:34 +0800

    c

--- client/java/chatRoomClient/src/chatRoomClient/JavaChatRoomLoginGUI.java
+++ client/java/chatRoomClient/src/chatRoomClient/JavaChatRoomLoginGUI.java
@@ -21,39 +21,49 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class JavaChatRoomLoginGUI extends JFrame {
-    JLabel label1 = new JLabel("Server IP: ");
-    JLabel label2 = new JLabel("Port: ");
-    JLabel label3 = new JLabel("User ID: ");
-    JLabel label4 = new JLabel("User Password: ");
-    JTextField textField1 = new JTextField(20);
-    JTextField textField2 = new JTextField(20);
-    JTextField textField3 = new JTextField(20);
-    JTextField textField4 = new JTextField(20);
+    JLabel labelServer = new JLabel("Server IP: ");
+    JLabel labelPort = new JLabel("Port: ");
+    JLabel labelID = new JLabel("User ID: ");
+    JLabel labelPassword = new JLabel("User Password: ");
+    JTextField textFieldServer = new JTextField(20);
+    JTextField textFieldPort = new JTextField(20);
+    JTextField textFieldID = new JTextField(20);
+    JTextField textFieldPassword = new JTextField(20);
     JButton button1 = new JButton("Login");
     JButton button2 = new JButton("Go to register page");
 
     public JavaChatRoomLoginGUI() {
         super("Chat room login");
         setResizable(false);
-        setLayout(new FlowLayout(FlowLayout.LEFT, 20, 10));
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         setSize(400, 250);
-        add(label1);
-        add(textField1);
-        add(label2);
-        add(textField2);
-        add(label3);
-        add(textField3);
-        add(label4);
-        add(textField4);
-        add(button1);
+        JPanel panelServer = new JPanel();
+        addComponentToPanel(panelServer, labelServer, textFieldServer);
+        add(panelServer);
+        JPanel panelPort = new JPanel();
+        addComponentToPanel(panelPort, labelPort, textFieldPort);
+        add(panelPort);
+        JPanel panelID = new JPanel();
+        addComponentToPanel(panelID,labelID,textFieldID);
+        add(panelID);
+        JPanel panelPassword = new JPanel();
+        addComponentToPanel(panelPassword,labelPassword,textFieldPassword);
+        add(panelPassword);
+        JPanel panelButton = new JPanel();
+        panelButton.setLayout(new FlowLayout());
+        panelButton.add(button1);
         button2.addActionListener(new RegisterActionListener());
-        add(button2);
+        panelButton.add(button2);
+        add(panelButton);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(JavaChatRoomLoginGUI::new);
+    private void addComponentToPanel(JPanel panel, JLabel label,
+                                     JTextField textField) {
+        panel.setLayout(new FlowLayout());
+        panel.add(label);
+        panel.add(textField);
     }
 
     private static class RegisterActionListener implements ActionListener {
