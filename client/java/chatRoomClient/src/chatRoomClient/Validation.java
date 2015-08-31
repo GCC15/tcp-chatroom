b94537d76a1c0d4e35238c59b423f6a7ea12764d
Zhang NS <zhanglinghan120@gmail.com>
2015-09-01 00:01:14 +0800

    \w

--- client/java/chatRoomClient/src/chatRoomClient/Validation.java
+++ client/java/chatRoomClient/src/chatRoomClient/Validation.java
@@ -85,7 +85,7 @@ public final class Validation {
         if (length < 1 || length > 16) {
             throw new InvalidIdLengthException();
         }
-        if (!id.matches("^[a-zA-Z0-9_]*$")){
+        if (!id.matches("^\\w*$")){
             throw new InvalidIdCharException();
         }
     }
